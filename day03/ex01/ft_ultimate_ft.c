99f9cf9fe54b08052f6f939d09b7ba28bcb757ce
GMNSA <kohotokk@mail.ru>
2019-04-07 16:18:47 +0000

    exercise 01

---
+++ day03/ex01/ft_ultimate_ft.c
@@ -0,0 +1,11 @@
+// Text File
+// AUTHOR:   _who
+// FILE:     ft_ultimate_ft.c
+// ROLE:     TODO (some explanation)
+// CREATED:  2019-04-07 16:17:12
+// MODIFIED: 2019-04-07 16:18:10
+
+void    ft_ultimate_ft(int *********nbr)
+{
+    *********nbr = 42;
+}
