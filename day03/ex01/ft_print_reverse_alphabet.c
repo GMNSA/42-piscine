3cfe4b00496b47ac240e07f94db23c0c2fea2d15
GMNSA <kohotokk@mail.ru>
2019-04-07 15:48:20 +0000

    ex00 modify and exercise 01

--- day03/ex01/ft_print_reverse_alphabet.c
+++ day03/ex01/ft_print_reverse_alphabet.c
@@ -1,9 +1,10 @@
 // Text File
 // AUTHOR:   _who
-// FILE:     ft_print_alphabet.c
+// FILE:     ft_print_reverse_alphabet.c
 // ROLE:     TODO (some explanation)
-// CREATED:  2019-04-07 14:22:21
-// MODIFIED: 2019-04-07 15:47:42
+// CREATED:  2019-04-07 15:45:53
+// MODIFIED: 2019-04-07 15:46:13
+
 
 #include <unistd.h>
 
@@ -12,19 +13,19 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void    ft_print_alphabet(void)
+void    ft_print_reverse_alphabet(void)
 {
     char    start;
     char    stop;
     char    temp;
 
 
-    start = 97;
-    stop = 122;
+    start = 122;
+    stop = 97;
     temp = start;
-    while (temp != (stop + 1))
+    while (temp != (stop - 1))
     {
         ft_putchar(temp);
-        temp += 1;
+        temp -= 1;
     }
 }
