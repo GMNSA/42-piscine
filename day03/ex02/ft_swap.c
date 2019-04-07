e06ba721f4c2f11cd5a11287d10c279e2bb12f85
GMNSA <kohotokk@mail.ru>
2019-04-07 16:23:35 +0000

    exercise 02

---
+++ day03/ex02/ft_swap.c
@@ -0,0 +1,15 @@
+// Text File
+// AUTHOR:   _who
+// FILE:     ft_swap.c
+// ROLE:     TODO (some explanation)
+// CREATED:  2019-04-07 16:19:42
+// MODIFIED: 2019-04-07 16:23:07
+
+void    ft_swap(int *a, int *b)
+{
+    int     temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
