a4f30da4e989ea503ad227b185a5aa814a3fb71f
GMNSA <kohotokk@mail.ru>
2019-04-07 16:28:19 +0000

    exercise 03

---
+++ day03/ex03/ft_div_mod.c
@@ -0,0 +1,12 @@
+// Text File
+// AUTHOR:   _who
+// FILE:     ft_div_mod.c
+// ROLE:     TODO (some explanation)
+// CREATED:  2019-04-07 16:24:03
+// MODIFIED: 2019-04-07 16:27:58
+
+void    ft_div_mod(int a, int b, int *div, int *mod)
+{
+    *div = a / b;
+    *mod = a % b;
+}
