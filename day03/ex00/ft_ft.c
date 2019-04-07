ac151df49fb2461d27646571f3483974e02b3fe8
GMNSA <kohotokk@mail.ru>
2019-04-07 16:16:34 +0000

    exercise ex00

---
+++ day03/ex00/ft_ft.c
@@ -0,0 +1,11 @@
+// Text File
+// AUTHOR:   _who
+// FILE:     ft_ft.c
+// ROLE:     TODO (some explanation)
+// CREATED:  2019-04-07 16:13:29
+// MODIFIED: 2019-04-07 16:16:08
+
+void    ft_ft(int *nbr)
+{
+    *nbr = 42;
+}
