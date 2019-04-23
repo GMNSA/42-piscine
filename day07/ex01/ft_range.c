3bb58426cc07050cf43bb4c44ea0dc73c45f0253
GMNSA <kohotokk@mail.ru>
2019-04-23 17:15:32 +0000

    exercise 01

---
+++ day07/ex01/ft_range.c
@@ -0,0 +1,20 @@
+/* ft_ranger.c */
+
+#include <stdlib.h>
+
+int     *ft_range(int min, int max)
+{
+    int     *num;
+    int     i;
+
+    if (!(num = (int*)malloc((sizeof(num) * (max - min)))))
+        return (0);
+    i = 0;
+    while (min < max)
+    {
+        num[i] = min;
+        ++i;
+        ++min;
+    }
+    return (num);
+}
