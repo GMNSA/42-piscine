8e8b6147e5fa59a218beba4c91e38f150668a645
GMNSA <kohotokk@mail.ru>
2019-04-24 04:40:12 +0000

    add check greater or equal

--- day07/ex01/ft_range.c
+++ day07/ex01/ft_range.c
@@ -7,6 +7,11 @@ int     *ft_range(int min, int max)
     int     *num;
     int     i;
 
+    if (min >= max)
+    {
+        num = &min;
+        return (num);
+    }
     if (!(num = (int*)malloc((sizeof(num) * (max - min)))))
         return (0);
     i = 0;
